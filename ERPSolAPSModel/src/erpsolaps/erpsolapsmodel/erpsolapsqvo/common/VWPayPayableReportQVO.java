c2e36a93d9d20fbf3254890740a5697bc60c0229
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2022-05-23 10:04:08 +0500

    adding auto suggest for payable unsubmit

--- ERPSolAPSModel/src/erpsolaps/erpsolapsmodel/erpsolapsqvo/common/VWPayPayableReportQVO.java
+++ ERPSolAPSModel/src/erpsolaps/erpsolapsmodel/erpsolapsqvo/common/VWPayPayableReportQVO.java
@@ -7,5 +7,7 @@ import oracle.jbo.ViewObject;
 // ---------------------------------------------------------------------
 public interface VWPayPayableReportQVO extends ViewObject {
     void doSetPayableReportDefaults();
+
+    void doSetERPSolAPDocumentUnsubmit();
 }
 
